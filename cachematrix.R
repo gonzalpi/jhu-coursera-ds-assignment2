aa6fe9bea38ca7609f4a779b549413ef5d139b80
KarlKarpacho <131783973+KarlKarpacho@users.noreply.github.com>
2023-08-04 15:11:46 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,77 @@
-## Create a list object containing a matrix and its cached inverse
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
 
-## Store a matrix in a list object that contains its inverse too
-## This function is virtually the same as makeVector in README.md
 
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+
+
+
+
+setwd("/Users/macbook/laba2/1")
+getwd()
+
+# makeCacheMatrix: Эта функция создает специальный "матричный" объект,
+# который может кешировать свою обратную матрицу.
 makeCacheMatrix <- function(x = matrix()) {
-            inv <- NULL
-            set <- function(y) {
-                    x <<- y
-                    inv <<- NULL
-            }
-            get <- function() x
-            setinverse <- function(inverse) inv <<- inverse
-            getinverse <- function() inv
-            list(set = set, get = get,
-                 setinverse = setinverse,
-                 getinverse = getinverse)
+  i <- NULL # хранит обратную матрицу
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() {x}
+  setinverse <- function(inverse) { 
+    # устанавливает значение обратной матрицы и очищает предыдущее значение
+    # Оператор "<<-" используется для установки переменной, которая уже существует
+    i <<- inverse
+  }
+  getinverse <- function() {i}
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Retrieve the inverse of a matrix and calculate if not already stored
-
+# cacheSolve: Эта функция вычисляет обратную матрицу для специального "матричного" 
+# объекта, возвращенного функцией makeCacheMatrix выше. Если обратная матрица 
+# уже была рассчитана (и матрица не изменилась), то функция cacheSolve 
+# извлекает обратную матрицу из кеша.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        inv <- x$getinverse()
-        if(!is.null(inv)) {
-                message("getting cached data")
-                return(inv)
-        }
-        data <- x$get()
-        inv <- solve(data, ...)
-        x$setinverse(inv)
-        inv
+  ## Возвращает матрицу, которая является обратной для 'x'
+  i <- x$getinverse()
+  if (!is.null(i)) {
+    message("извлекаю данные из кеша")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i # возвращаем обратную матрицу
 }
+
+# Пример использования программы
+Mat <- matrix(c(1:8),3,3)
+Mat
+Mat1 <- makeCacheMatrix(Mat)
+Mat1
+cacheSolve(Mat1)
